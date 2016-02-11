a8127192b75ded3fea5eff4c961023c4efdb0657
Elijah McPherson <emcpherson@undergroundelephant.com>
2016-02-11 00:01:30 -0800

    Adds migration for creating the pipeline_execution_settings table.

---
+++ src/db/migrations/20160210000000_pipeline_extension_settings_table.js
@@ -0,0 +1,24 @@
+'use strict'
+
+let timestamps = require('../timestamps-schema')
+
+exports.up = function (knex, Promise) {
+
+  return knex.schema.createTable('pipeline_execution_settings', function (table) {
+    table.increments('id')
+    table.integer('pipeline_config_id').unsigned().references('pipeline_configs.id')
+    table.string('vendor')
+    table.string('extension_id')
+    table.string('category_id')
+    table.string('key')
+    table.text('value')
+    timestamps(knex, table)
+  })
+
+}
+
+exports.down = function (knex, Promise) {
+
+  return knex.schema.dropTable('pipeline_execution_settings')
+
+}
