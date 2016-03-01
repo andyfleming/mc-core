e70fbe542c42ce3c45ce6322902141176e2f6839
Andy Fleming <andy@andyfleming.com>
2016-02-29 20:50:54 -0800

    Merging extension setting structures with values

--- src/api/controllers/pipeline-extension-settings.js
+++ src/api/controllers/pipeline-extension-settings.js
@@ -5,29 +5,35 @@ const PIPELINE_EXTENSION_SETTINGS = 'pipeline_extension_settings'
 //let connection = require('../../db/connection')
 const success = require('../utils/responses/success')
 const error = require('../utils/responses/error')
-const query = require('../../db/queries')
+const connection = require('../../db/connection')
+const registry = require('../../extensions/registry')
+const _ = require('lodash')
 
 module.exports = {
 
   /**
-   * Fetches a list of pipeline settings
+   * Fetches a list of pipeline settings (values)
    *
    * @param req
    * @param res
    */
   getList: (req, res) => {
-    query.all(PIPELINE_EXTENSION_SETTINGS)
-      .where('pipeline_config_id', req.params.id)
+    connection.select()
+      .where('pipeline_config_id', +req.params.pipeline_id)
+      .from(PIPELINE_EXTENSION_SETTINGS)
       .then(rows => {
         let settings = {}
         rows.forEach(setting => {
           const ext = setting.extension_id
           const cat = setting.category_id
-          settings[ext] = setting[ext] || {}
-          settings[ext][cat] = setting[ext][cat] || {}
-          settings[ext][cat][setting.key] = setting.value
+          settings[ext] = settings[ext] || {}
+          settings[ext][cat] = settings[ext][cat] || {}
+          settings[ext][cat].settings = settings[ext][cat].settings || {}
+          settings[ext][cat].settings[setting.key] = settings[ext][cat].settings[setting.key] || {}
+          settings[ext][cat].settings[setting.key].value = setting.value
         })
-        return settings
+
+        return _.defaultsDeep({}, registry.getPipelineSettingsGroups(), settings)
       })
       .then(success.bind(res))
       .catch(error.bind(res))
